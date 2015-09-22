bbb935e8a8d31ca7c418cc21d6b83a679a465bb6
marialangman <g00227867@stu.raritanval.edu>
2015-09-21 21:33:02 -0400

    add a new private field with appropriate accessors

--- Book.java
+++ Book.java
@@ -3,25 +3,28 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @Maria Langman
+ * @September 21, 2015
  */
 class Book
 {
     // The fields.
     private String author;
     private String title;
+    private String publisher;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, String bookPublisher)
     {
         author = bookAuthor;
         title = bookTitle;
+        publisher = bookPublisher;
+
     }
-    
+
     // Return author
     public String getAuthor()
     {
@@ -32,4 +35,20 @@ class Book
     {
         return title;
     }
+
+    public String getPublisher()
+    {
+        return publisher;
+    }
+    public void printBook()
+    {
+        System.out.println("Author: " + author + ", " + "Title: " + title +"\nPublisher: " + publisher);
+    }
+
+    public void printDetails()
+    {
+		System.out.println("Author: "+ author);
+		System.out.println("Title: "+ title);
+		System.out.println("Publisher: "+ publisher);
+	}
 }
